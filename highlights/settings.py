1c2851378bb6d63fd984f85dbc21749a81482ac4
Paul Vidal <paul.vidal@yahoo.fr>
2017-05-01 19:40:00 +0200

    change location of messenger middleware

--- highlights/settings.py
+++ highlights/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'fb_highlights.middleware.messenger_middleware.MessengerMiddleware'
+    'fb_bot.middleware.messenger_middleware.MessengerMiddleware'
 ]
 
 ROOT_URLCONF = 'highlights.urls'

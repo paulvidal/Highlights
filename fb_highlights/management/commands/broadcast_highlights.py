9dbafa393aeabca1fefff79b10fbd0be89c90caf
Paul Vidal <paul.vidal@yahoo.fr>
2018-01-11 17:51:23 +0000

    add sentry logging when executing scheduled task

--- fb_highlights/management/commands/broadcast_highlights.py
+++ fb_highlights/management/commands/broadcast_highlights.py
@@ -1,9 +1,16 @@
+from raven.contrib.django.raven_compat.models import client
 from django.core.management.base import BaseCommand
 
+from highlights import settings
 from fb_bot import scheduler
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        scheduler.send_most_recent_highlights()
+        try:
+            scheduler.send_most_recent_highlights()
+        except:
+            # Report to sentry if problem detected
+            if not settings.DEBUG:
+                client.captureException()

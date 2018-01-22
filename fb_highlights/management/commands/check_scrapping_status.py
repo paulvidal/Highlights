ae0acc91797083edabfa37461229876aaa8a4ef6
Paul Vidal <paul.vidal@yahoo.fr>
2018-01-22 17:42:28 +0000

    add scrapping status check

---
+++ fb_highlights/management/commands/check_scrapping_status.py
@@ -0,0 +1,31 @@
+from raven.contrib.django.raven_compat.models import client
+from django.core.management.base import BaseCommand
+
+from fb_bot.highlight_fetchers import footyroom_fetcher, hoofoot_fetcher
+from highlights import settings
+
+
+class ScrappingException(Exception):
+    pass
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        try:
+            highlights_footyroom = footyroom_fetcher.fetch_highlights(num_pagelet=1, max_days_ago=2)
+
+            if not highlights_footyroom:
+                raise ScrappingException("Failed to scrape FOOTYROOM")
+
+            highlights_hoofoot = hoofoot_fetcher.fetch_highlights(num_pagelet=1, max_days_ago=2)
+
+            if not highlights_hoofoot:
+                raise ScrappingException("Failed to scrape HOOFOOT")
+
+        except Exception as error:
+            if not settings.DEBUG:
+                # Report to sentry if problem detected
+                client.captureException()
+            else:
+                raise error

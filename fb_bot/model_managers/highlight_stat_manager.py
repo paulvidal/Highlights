2489efcea13576b828696dba3a1f7a2fdedf270a
Paul Vidal <paul.vidal@yahoo.fr>
2018-01-25 11:55:24 +0000

    fix double insert of highlight stat

--- fb_bot/model_managers/highlight_stat_manager.py
+++ fb_bot/model_managers/highlight_stat_manager.py
@@ -1,4 +1,6 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+
+import dateparser
 
 from fb_bot.model_managers import user_manager
 from fb_highlights.models import HighlightStat
@@ -6,7 +8,21 @@ from fb_highlights.models import HighlightStat
 
 def add_highlight_stat(fb_id, highlight_model):
     user = user_manager.get_user(fb_id)
-    time = str(datetime.today())
+    time = datetime.today()
+
+    highlight_stats = HighlightStat.objects.filter(user=user,
+                                                   team1=highlight_model.team1,
+                                                   score1=highlight_model.score1,
+                                                   team2=highlight_model.team2,
+                                                   score2=highlight_model.score2,
+                                                   link=highlight_model.link)
+
+    # Do not insert 2 time the same event if there are only 5 seconds difference
+    for highlight_stat in highlight_stats:
+        highlight_time = dateparser.parse(highlight_stat.time)
+
+        if abs(highlight_time - time) < timedelta(seconds=5):
+            return
 
     HighlightStat.objects.update_or_create(user=user,
                                            team1=highlight_model.team1,
@@ -14,4 +30,4 @@ def add_highlight_stat(fb_id, highlight_model):
                                            team2=highlight_model.team2,
                                            score2=highlight_model.score2,
                                            link=highlight_model.link,
-                                           time=time)
+                                           time=str(time))

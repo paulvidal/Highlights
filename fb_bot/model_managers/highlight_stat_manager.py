c81a2cf7d06e299c19a0c586b81145fc27c63407
Paul Vidal <paul.vidal@yahoo.fr>
2018-02-22 12:50:09 +0000

    change highlight click stat to 5 mins

--- fb_bot/model_managers/highlight_stat_manager.py
+++ fb_bot/model_managers/highlight_stat_manager.py
@@ -15,11 +15,11 @@ def add_highlight_stat(fb_id, highlight_model):
                                                    score2=highlight_model.score2,
                                                    link=highlight_model.link)
 
-    # Do not insert 2 time the same event if there are only 5 seconds difference
+    # Do not insert 2 time the same event if there are only 5 minutes difference
     for highlight_stat in highlight_stats:
         highlight_time = highlight_stat.time
 
-        if abs(highlight_time - time) < timedelta(seconds=5):
+        if abs(highlight_time - time) < timedelta(minutes=5):
             return
 
     HighlightStat.objects.update_or_create(user=user,

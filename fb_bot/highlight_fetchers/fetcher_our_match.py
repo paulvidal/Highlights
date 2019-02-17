e6faee70242b361f3c1219bb98b04477182ad4af
Paul Vidal <paul.vidal14@imperial.ac.uk>
2019-02-17 22:57:14 +0000

    fix bug in outmatch fetching

--- fb_bot/highlight_fetchers/fetcher_our_match.py
+++ fb_bot/highlight_fetchers/fetcher_our_match.py
@@ -193,7 +193,7 @@ def _get_video_links(soup):
             regex = "\'type\':\'(.*?)\'"
             types = re.compile(regex, 0).findall(script_text)
 
-            previous_type = None
+            previous_type = 'normal'
 
             for i in range(len(types)):
 
@@ -261,7 +261,7 @@ if __name__ == "__main__":
     print("\nFetch highlights ------------------------------ \n")
 
     start_time = time.time()
-    highlights = fetch_highlights(num_pagelet=1, max_days_ago=40)
+    highlights = fetch_highlights(num_pagelet=4, max_days_ago=40)
 
     for highlight in highlights:
         print(highlight)

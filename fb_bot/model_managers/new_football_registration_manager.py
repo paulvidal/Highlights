b17143d899f867e902e862761b54f3373bef85a3
Paul Vidal <paul.vidal@yahoo.fr>
2019-05-07 20:42:53 +0200

    fix bug when inserting new football registration

--- fb_bot/model_managers/new_football_registration_manager.py
+++ fb_bot/model_managers/new_football_registration_manager.py
@@ -1,7 +1,12 @@
+from fb_bot.logger import logger
 from fb_highlights.models import NewFootballRegistration
 
 
 def add_football_registration(name, source, user=None):
-    NewFootballRegistration.objects.update_or_create(name=name,
-                                                     source=source,
-                                                     user=user)
+    try:
+        NewFootballRegistration.objects.update_or_create(name=name,
+                                                         source=source,
+                                                         user=user)
+    except:
+        # TODO: update this with a proper create without private key and tolerating duplicates
+        logger.error("Failed to add new football registration")

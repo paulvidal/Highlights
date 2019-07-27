bc89e489d081f3e9a050f15f7b93481238a7cc78
Paul Vidal <paul.vidal@yahoo.fr>
2019-07-27 18:52:47 +0100

    fix hoofoot fetching

--- fb_bot/highlight_fetchers/info/providers.py
+++ fb_bot/highlight_fetchers/info/providers.py
@@ -4,8 +4,9 @@ OK_RU = 'ok.ru'
 FOOYTROOM = 'footyroom.com'
 YOUTUBE = 'youtube.com'
 
-# Similar format, different hosting gplatform
+# Similar format, different hosting platform
 CONTENT_VENTURES = 'content-ventures.com'
 MATCHAT_ONLINE = 'matchat.online'
 VIDEO_STREAMLET = 'videostreamlet.net'
 VEUCLIPS = 'veuclips.com'
+VIUCLIPS = 'viuclips.net'

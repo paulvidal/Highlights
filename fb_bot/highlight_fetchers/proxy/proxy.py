ccede64f1866208feb35d788f94015677e24e938
Paul Vidal <paul.vidal@yahoo.fr>
2019-10-03 22:30:44 +0200

    improve scraper api logging

--- fb_bot/highlight_fetchers/proxy/proxy.py
+++ fb_bot/highlight_fetchers/proxy/proxy.py
@@ -18,19 +18,21 @@ def get(url, render=False):
         )
 
         if response.status_code == requests.codes.unauthorized:
-            logger.log('Scrapper API key invalid - you should remove it: ' + key.code, forward=True)
+            logger.warning('Scrapper API key invalid - you should remove it: ' + key.code)
             continue
 
         if response.status_code == requests.codes.forbidden:
-            logger.log('Scrapper API key too many requests: ' + key.code, forward=True)
+            logger.warning('Scrapper API key too many requests: ' + key.code)
             scraper_api_key_manager.invalidate_key(key)
             continue
 
         if response.status_code == requests.codes.ok and not key.valid:
-            logger.log('Scrapper API key reset: ' + key.code, forward=True)
+            logger.warning('Scrapper API key reset: ' + key.code)
             scraper_api_key_manager.validate_key(key)
 
         return response
+
+    logger.critical('Scrapper API - all keys are invalid')
 
 
 def post(url, data):
@@ -41,19 +43,21 @@ def post(url, data):
         )
 
         if response.status_code == requests.codes.unauthorized:
-            logger.log('Scrapper API key invalid - you should remove it: ' + key.code, forward=True)
+            logger.warning('Scrapper API key invalid - you should remove it: ' + key.code)
             continue
 
         if response.status_code == requests.codes.forbidden:
-            logger.log('Scrapper API key too many requests: ' + key.code, forward=True)
+            logger.warning('Scrapper API key too many requests: ' + key.code)
             scraper_api_key_manager.invalidate_key(key)
             continue
 
         if response.status_code == requests.codes.ok and not key.valid:
-            logger.log('Scrapper API key reset: ' + key.code, forward=True)
+            logger.warning('Scrapper API key reset: ' + key.code)
             scraper_api_key_manager.validate_key(key)
 
         return response
+
+    logger.critical('Scrapper API - all keys are invalid')
 
 
 def form_url(url):
